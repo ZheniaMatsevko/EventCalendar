8cb3a62bf150cd6972dbaede626360c6aebce916
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2023-12-01 20:22:12 +0200

    add security configuration

---
+++ src/main/java/com/system/eventcalendar/security/SecurityConfiguration.java
@@ -0,0 +1,21 @@
+package com.system.eventcalendar.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(x -> x.disable())
+                .authorizeHttpRequests(authorize -> authorize
+                        .anyRequest().permitAll()
+                );
+
+        return http.build();
+    }
+}
