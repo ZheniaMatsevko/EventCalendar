6f0e8118bc7da9f3702f94ec8d89e996407ad457
Yevheniia Matsevko <mazevkozhenia@gmail.com>
2023-12-04 21:18:59 +0200

    update Program_structure.pdf

--- src/main/java/com/system/eventcalendar/EventCalendarApplication.java
+++ src/main/java/com/system/eventcalendar/EventCalendarApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-/*TODO
-*  7. Look through all files - think how to improve
-* */
+
 @SpringBootApplication
 @ServletComponentScan
 public class EventCalendarApplication {
